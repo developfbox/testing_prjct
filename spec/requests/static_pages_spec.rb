55edb8bff988393c2c22b162c32b4c4c85324867
sergey_ermolaev <sergeyfbox@gmail.com>
2016-11-05 11:14:20 +0300

    add_dinamics_in_static_pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+describe "Static pages" do
+  describe "Home page" do
+    it "should have the content 'Testing home page'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Testing home page')
+    end
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Home")
+    end
+  end
+  describe "Help page" do
+    it "should have the content 'Testing help page'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Testing help page')
+    end
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Help")
+    end
+  end
+  describe "About page" do
+    it "should have the content 'Testing about us page'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('Testing about us page')
+    end
+    it "should have the title 'About us'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("About us")
+    end
+  end
+end
