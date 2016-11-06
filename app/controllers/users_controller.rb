933d153ffb438f65f09ca34d6b0badcb23a7c752
sergey_ermolaev <sergeyfbox@gmail.com>
2016-11-06 22:44:55 +0300

    add_user_signup_backend_6_11

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,19 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Testing app!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password,
+                                   :password_confirmation)
+  end
 end
